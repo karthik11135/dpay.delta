b9fa840179c5fc675827a28c94d3cd1b36b6a69a
Karthik Rishinarada <karthikrk11135@gmail.com>
2024-09-29 11:59:23 +0530

    done finally

--- src/components/BalanceCard.tsx
+++ src/components/BalanceCard.tsx
@@ -1,19 +1,21 @@
-
-
 const BalanceCard = () => {
+
+  const randNum =
+    Math.floor(Math.floor(Math.random() * (151 - 100) + 100) / 10) * 10;
+
   return (
     <div className="py-3 my-3 px-6 bg-bgColorFrend rounded-lg">
-        <div className='grid grid-cols-12 gap-4'>
-            <div className='col-span-7 py-4'>
-                <h2 className='text-white text-2xl mb-2'>Balance Left</h2>
-                <p className='text-white text-3xl'>&#8377; 110</p>
-            </div>
-            <div className='col-span-5'>
-                <img src="/balancepic.jpg"></img>
-            </div>
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-7 py-4">
+          <h2 className="text-white text-2xl mb-2">Balance Left</h2>
+          <p className="text-white text-3xl">&#8377; {randNum}</p>
         </div>
+        <div className="col-span-5">
+          <img src="/balancepic.jpg"></img>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BalanceCard
+export default BalanceCard;
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,25 @@
-
-
 const TopBar = () => {
+  const names = [
+    "A. Ranvindar N",
+    "J. Mahesh Ram",
+    "A. Raju Mohan",
+    "M. Surya Rahul",
+  ];
+  const randomNum = Math.floor(Math.random() * 4);
+
   return (
     <div className="py-4 flex">
       <div className="text-3xl w-full text-wrap py-2.5">
         <div className="px-3 leading-10 text-white">
           Hello , <br></br>
-          <span className="text-greenColor">J. Tharsath Babu</span>
+          <span className="text-greenColor">{names[randomNum]}</span>
         </div>
       </div>
       <div className="ms-auto bg-blue-200 border-none">
-        <img src={"/festlogo.jpg"} className="w-28  border-none h-28 object-fill" />
+        <img
+          src={"/festlogo.jpg"}
+          className="w-28  border-none h-28 object-fill"
+        />
       </div>
     </div>
   );
